cf921a71a2847f2389208e472dff38ebe42b3565
Danijel-James Wynyard <danijel@repasscloud.com>
2023-08-08 12:33:20 +1000

    update twitterxbot

--- run_tweepy.py
+++ run_tweepy.py
@@ -1,4 +1,6 @@
 import os
+import csv
+from datetime import datetime
 import tweepy
 
 api_key = os.environ.get('TWITTER_CONSUMER_KEY')
@@ -12,10 +14,28 @@ client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_t
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-with open('current_tweet', 'r') as file:
-    thisTweet = file.read()
+# Get the current date in the format YYYYMMDD
+current_date = datetime.now().strftime('%Y%m%d')
 
-print(thisTweet)
+# Get the current meridian (AM or PM)
+current_meridian = datetime.now().strftime('%p')
 
-client.create_tweet(text=thisTweet)
+# Initialize thisTweet to None
+thisTweet = None
+
+# Read the CSV file
+with open('tweets.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        if row['DATE'] == current_date and row['MERIDIAN'] == current_meridian:
+            thisTweet = row['TWEET']
+            break
+
+# Check if thisTweet is None and handle it
+if thisTweet is not None:
+    client.create_tweet(text=thisTweet)
+else:
+    print("No matching tweet found for the current date and meridian.")
+
+
 
